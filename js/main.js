5fee72502bf61fe1eea78a8c44413984b511d323
bakkar1 <mbarkbakkar1@gmail.com>
2020-12-27 19:21:12 +0100

    ji

--- js/main.js
+++ js/main.js
@@ -2,27 +2,25 @@
 
 var clickI = document.querySelector('.fa-bars'),
     headerUl = document.querySelector('nav ol li ul'),
-    headerUlLi = document.querySelectorAll('nav ol li ul li');
+    headerUlLi = document.querySelectorAll('nav ol li ul li'),
+    body = document.querySelector("body");
 
-function noScroll(){
-    window.scrollTo(0,0);
-}
+
 clickI.onclick = function(){
     headerUl.classList.toggle('showUl');
     if(clickI.getAttribute('class') == 'fas fa-bars visibale-xs visibale-sm'){
-        window.addEventListener('scroll', noScroll);
+        body.classList.add("hidden-Y");
         clickI.setAttribute('class', 'fas fa-times visibale-xs visibale-sm');
     }
     else{
-        window.removeEventListener('scroll', noScroll);
+        body.classList.remove("hidden-Y");
         clickI.setAttribute('class', 'fas fa-bars visibale-xs visibale-sm');
     }
 }
 headerUlLi.forEach(li =>{
     li.onclick = function(){
-        window.removeEventListener('scroll',noScroll);
+        body.classList.remove("hidden-Y");
         headerUl.classList.remove('showUl');
         clickI.setAttribute('class', 'fas fa-bars visibale-xs visibale-sm');
-        
     }
 });
